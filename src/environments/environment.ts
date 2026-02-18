ae8a3d426e2bbf2443652972b36921aa10fd9c2c
Sunil Kumar Majhi <techsunil037@gmail.com>
2026-02-18 13:48:02 +0530

    fix: Update Firebase configuration in development environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 // Development environment configuration
 export const environment = {
-    production: false,
-    environmentName: 'development',
-    firebase: {
-        apiKey: "AIzaSyD8azzlocIj3Y1tOKRezFqdL3vOI8jPQbU",
-        authDomain: "ppms-b8d2b-dev.firebaseapp.com",
-        projectId: "ppms-b8d2b-dev",
-        storageBucket: "ppms-b8d2b-dev.firebasestorage.app",
-        messagingSenderId: "361250131125",
-        appId: "1:361250131125:web:657bf846c213266f7d42fc"
-    }
+  production: false,
+  environmentName: 'development',
+  firebase: {
+    apiKey: 'AIzaSyARh7IJBpZ8xoIiGTEqctkSfflP2aXtBbs',
+    authDomain: 'ppms-dev-9361e.firebaseapp.com',
+    projectId: 'ppms-dev-9361e',
+    storageBucket: 'ppms-dev-9361e.firebasestorage.app',
+    messagingSenderId: '578579666497',
+    appId: '1:578579666497:web:4ccfb169dde3c3a97918c0',
+  },
 };
